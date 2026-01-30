c212982b847c6232f5f385a6d14e5ed4c57437e1
VEGABOND <100884457+6VE6ABOND6@users.noreply.github.com>
2026-01-31 02:46:16 +0300

    Update engine_tr.py

--- engine_tr.py
+++ engine_tr.py
@@ -1,39 +1,37 @@
 import itertools
 import datetime as T
-import diax as d
 
 todayYear=int(T.date.today().year)
 
 
-def engineHearh():
- name=str(d.user["Name"])
- secondName=str(d.user["SecondName"])
- yearBirth=str(d.user["birthYear"])
+def engineHearth(name,secondName,yearBirth):
  #name+symbol
- nameGen(name,secondName,yearBirth)
+ yield from nameGen(name,secondName,yearBirth)
  #name+team+symbol
- teamGen(name)
- teamGen(name.capitalize())
+ yield from teamGen(name)
+ yield from teamGen(name.capitalize())
  #name+plate+#symbol
- plateGen(name)
+ yield from plateGen(name)
     
 
 def symbolGen(word):
     
     symbols=["_",".","-","!"]
-    
-    for sembol in symbols:
-     if word[-4:].isdigit() or word[:-4].isdigit():   
-         
-      yield word[:-4]+sembol+word[-4:]
-      
-     elif word[:-2].isdigit():
-         
-      yield word[-2:]+sembol+word[:-2]
-      
-      
-     yield word+sembol
-     yield sembol+word
+    for sembol in symbols:   
+     if word[-4:].isdigit():
+       yield word[:-4] + sembol + word[-4:] 
+        
+        
+     elif word[:4].isdigit():
+      yield word[:4] + sembol + word[4:]
+
+       
+     elif word[-2:].isdigit():
+        yield word[:-2] + sembol + word[-2:] 
+             
+       
+     yield word + sembol      
+     yield sembol + word 
      
 def nameGen(name,secondName,yearBirth):
  
@@ -61,7 +59,7 @@ def nameGen(name,secondName,yearBirth):
  
 def teamGen(name):
      teamNames=["gs","bjk","fb","ts","bsk","ksk","goz","samsun","ads","eses","bursa","cimbom","aslan","ultraslan","kanarya","sari","lacivert",
-                "kartal","kara","bordomavi","firtina","sarikirmizi","sari_lacivert","siyahbeyaz","bordomavi","yesilbeyaz"
+                "kartal","kara","bordomavi","firtina","sarikirmizi","sari_lacivert","siyahbeyaz",
      ]
      teamYears=["1905","1907","1903","1967","1963","1954","1965","1925","1912","1453","1881","1938"]
      
@@ -76,12 +74,12 @@ def teamGen(name):
        
      
 def plateGen(name):
-        for i in range(82): #plate
+        for i in range(1,82): #plate
          y=f"{i:02}"
     
         yield from symbolGen(y+name)
         yield from symbolGen(name+y)
-        
+
 
     
 
